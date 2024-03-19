1e443c3fc7f16bc184e3de088afcacdbabd48e58
dand <79340018+dZyad@users.noreply.github.com>
2024-03-19 14:30:41 -0300

    Add UI functionality

--- game.js
+++ game.js
@@ -72,6 +72,20 @@ function GameController(
         return;
     }
 
+    const checkTie = () => {
+        const currentBoard = board.getBoard();
+
+        for ( let i = 0; i < 3; i++ ) {
+            for ( let j = 0; j < 3; j++ ) {
+                if (currentBoard[i][j] === '') {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
     const playRound = (cell) => {
         console.log(
             `${getActivePlayer().name} place ${getActivePlayer().token} in cell [${cell}]`
@@ -79,10 +93,17 @@ function GameController(
         board.placeToken(cell, getActivePlayer().token);
 
         const winner = checkWinner();
+        const tie = checkTie();
 
         if (winner) {
             printNewRound();
             console.log(`${getActivePlayer().name} won!`);
+            return;
+        }
+
+        if (tie) {
+            printNewRound();
+            console.log(`It's a tie!`);
             return;
         }
 
@@ -92,7 +113,95 @@ function GameController(
 
     printNewRound();
 
-    return { playRound, getActivePlayer };
+    return { playRound, getActivePlayer, getBoard: board.getBoard, checkWinner, checkTie };
 }
 
-const game = GameController();
+function ScreenController() {
+    let game = GameController();
+    const playerTurnDiv = document.querySelector('#turn');
+    const cells = document.querySelectorAll('.cell');
+    const resultDiv = document.querySelector('#result');
+    const restartDiv = document.querySelector('#restart');
+
+    const updateScreen = () => {
+        if (!game.checkWinner() && !game.checkTie()) {
+            const activePlayer = game.getActivePlayer();
+            playerTurnDiv.textContent = `${activePlayer.name}'s turn...`;
+        }
+    };
+
+    const printResult = (result) => {
+        resultDiv.textContent = result;
+        endGame();
+    }
+
+    const restartGame = () => {
+        game = GameController();
+        restartDiv.removeChild(restartDiv.querySelector('button'));
+        updateScreen();
+        resultDiv.textContent = '';
+        cells.forEach(cell => {
+            cell.classList.add('active');
+            cell.textContent = '';
+        });
+        addClickableCells();
+    }
+
+    const endGame = () => {
+        const restartButton = document.createElement('button');
+        restartButton.textContent = 'Restart Game';
+        restartButton.id = 'restartButton';
+
+        cells.forEach(cell => {
+            cell.removeEventListener('click', clickHandlerBoard);
+        });
+
+        restartDiv.appendChild(restartButton);
+        restartButton.addEventListener('click', restartGame);
+        playerTurnDiv.textContent = '';
+    }
+
+    const playRoundAndUpdate = (cell) => {
+        const token = game.getActivePlayer().token;
+        game.playRound(cell);
+        updateScreen();
+
+        const winner = game.checkWinner();
+        const tie = game.checkTie();
+
+        if (winner) {
+            printResult(`${token} won!`);
+        } else if (tie) {
+            printResult(`It's a Tie!`);
+        }
+    }
+
+    function clickHandlerBoard(e) {
+        const token = game.getActivePlayer().token;
+        const cell = e.target;
+        if (!cell.classList.contains('active')) {
+            return;
+        }
+        
+        const cellId = cell.id;
+        const cellIndex = parseInt(cellId.split('-')[1]);
+        const row = Math.floor(cellIndex / 3);
+        const col = cellIndex % 3;
+
+        playRoundAndUpdate([row, col]);
+        cell.classList.remove('active');
+        cell.textContent = `${token}`;
+        updateScreen();
+    };
+
+    function addClickableCells() {
+        cells.forEach(cell => {
+            cell.addEventListener('click', clickHandlerBoard);
+        });
+    }
+
+    updateScreen();
+    addClickableCells();
+}
+
+ScreenController();
